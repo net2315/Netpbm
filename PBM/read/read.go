e382443cb94d72d2683ca560cc98935d35162b59
snetanel <netanel.serfaty@ynov.com>
2024-01-09 16:46:17 +0100

    create scanner

--- PBM/read/read.go
+++ PBM/read/read.go
@@ -1,6 +1,7 @@
 package Netpbm
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
@@ -13,10 +14,16 @@ type PBM struct {
 
 // ReadPBM reads a PBM image from a file and returns a struct that represents the image.
 func ReadPBM(filename string) (*PBM, error) {
+
+	//ouverture du fichier mis en parametre
 	path := "./" + filename
-	_, err := os.ReadFile(path)
+	file, err := os.ReadFile(path)
 	if err != nil {
 		fmt.Println(err)
 	}
-	
+
+	//lire la premiere ligne
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Text()
+
 }
