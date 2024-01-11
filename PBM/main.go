e29ad025176cd1c095948c02d1ff2d570a78ce23
snetanel <netanel.serfaty@ynov.com>
2024-01-11 16:34:09 +0100

    readPBM update

--- PBM/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-type PBM struct{
-    data [][]bool
-    width, height int
-    magicNumber string
-}
---
+++ PBM/read.go
@@ -0,0 +1,62 @@
+package Netpbm
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type PBM struct {
+	data          [][]bool
+	width, height int
+	magicNumber   string
+}
+func main()  {
+	ReadPBM("test1.pbm")
+}
+// ReadPBM lie l'image PBM du fichier et retourne dans la struct les infos de l'image.
+func ReadPBM(filename string) (*PBM, error) {
+	PBMstock := PBM{}
+	//ouverture du fichier mis en parametre
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	//Scanner le texte + conditions
+	scanner := bufio.NewScanner(file)
+	input := scanner.Text()
+	confirmOne := false
+	confirmTwo := false
+
+	for scanner.Scan() {
+		if strings.HasPrefix(input, "#") {
+			continue
+		} else if !confirmOne {
+			PBMstock.magicNumber = input
+			confirmOne = true
+		} else if confirmTwo {
+			sepa := strings.Fields(input)
+			if len(sepa)>2 {
+			width, errW := strconv.Atoi(sepa[0])
+			height, errH := strconv.Atoi(sepa[1])
+			if errW != nil || errH != nil {
+				return nil, fmt.Errorf("Erreur de conversion de largeur/hauteur: %v, %v", errW, errH)
+			}
+			PBMstock.width = width
+			PBMstock.height = height
+			confirmTwo = true
+			}
+		} 
+		if PBMstock.magicNumber = "P4" {
+			for i := 0; i < len(input); i++ {
+				
+			}
+		}
+	}
+	fmt.Printf("%v\n", PBMstock)
+	return &PBM{PBMstock.data,PBMstock.height,PBMstock.width,PBMstock.magicNumber}
+}
