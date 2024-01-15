e87e62e8ce80d59e39917260c898db0a39f7c0de
snetanel <netanel.serfaty@ynov.com>
2024-01-15 10:52:53 +0100

    add data

--- PBM/read.go
+++ PBM/read.go
@@ -14,7 +14,7 @@ type PBM struct {
 	magicNumber   string
 }
 
-func main() {
+func main()  {
 	ReadPBM("test1.pbm")
 }
 
@@ -42,32 +42,29 @@ func ReadPBM(filename string) (*PBM, error) {
 			confirmOne = true
 		} else if confirmTwo {
 			sepa := strings.Fields(input)
-			if len(sepa) > 2 {
-				width, errW := strconv.Atoi(sepa[0])
-				height, errH := strconv.Atoi(sepa[1])
-				if errW != nil || errH != nil {
-					return nil, fmt.Errorf("Erreur de conversion de largeur/hauteur: ", errW, errH)
-				}
-				PBMstock.width = width
-				PBMstock.height = height
-				PBMstock.data = make([][]bool, PBMstock.height)
-				for i := range PBMstock.data {
-					PBMstock.data[i] = make([]bool, PBMstock.width)
-				}
-				confirmTwo = true
+			if len(sepa)>2 {
+			width, errW := strconv.Atoi(sepa[0])
+			height, errH := strconv.Atoi(sepa[1])
+			if errW != nil || errH != nil {
+				return nil, fmt.Errorf("Erreur de conversion de largeur/hauteur: %v, %v", errW, errH)
 			}
-		}
-		for i := 0; i < PBMstock.height; i++ {
-			line := scanner.Text()
-			sepa := strings.Fields(line)
-			for j := 0; j < PBMstock.width; j++ {
-				if val, err := strconv.Atoi(sepa[j]); err == nil {
-					PBMstock.data[i][j] = val == 1
-				} 
+			PBMstock.width = width
+			PBMstock.height = height
+			confirmTwo = true
+			}
+		} 
+		if PBMstock.magicNumber == "P1" {
+			for i := 0; i < PBMstock.width; i++ {
+				for j := 1; j < PBMstock.height; j++ {
+					if PBMstock.data[i][j] == true {
+						fmt.Println("true")
+					} else {
+						fmt.Println("false")
+					}
+				}
 			}
 		}
 	}
 	fmt.Printf("%v\n", PBMstock)
-	return &PBM{PBMstock.data, PBMstock.height, PBMstock.width, PBMstock.magicNumber}, nil
-
+	return &PBM{PBMstock.data,PBMstock.height,PBMstock.width,PBMstock.magicNumber}, err
 }
