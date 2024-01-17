8b0a854129644166292454893301b2691c0c96e9
snetanel <netanel.serfaty@ynov.com>
2024-01-17 21:46:04 +0100

    add files PGM

---
+++ PGM/PGM.go
@@ -0,0 +1,13 @@
+package Netpbm
+
+type PGM struct{
+    data [][]uint8
+    width, height int
+    magicNumber string
+    max int
+}
+
+// ReadPGM reads a PGM image from a file and returns a struct that represents the image.
+func ReadPGM(filename string) (*PGM, error){
+    // ...
+}
